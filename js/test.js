8a14a220eb1f59a77c74aa2575f548feb3ffa1a8
lorak <k.zysk@zoho.com>
2015-07-31 17:24:50 +0200

    first ajax test page

---
+++ js/test.js
@@ -0,0 +1,50 @@
+(function() {
+
+
+	var GetWikiLinks = function() {
+        var self = this;
+
+        self.links = ko.observableArray();
+
+        self.getLinks = ko.computed(function() {
+
+            var wikiRequestLinksTimeout = setTimeout(function() {
+                    var $wikiElem = $('#wikipedia');
+                    $wikiElem.text('failed to get Wikipedia resources');
+                    console.log('failed to get Wikipedia resources');
+                }, 5000);
+
+            var wikiQuery = "Warsaw",
+            dt = 'jsonp',
+            wikiBase = 'http://en.wikipedia.org/w/api.php',
+            wikiUrl = wikiBase + '?action=opensearch&search=' + wikiQuery + '&format=json&callback=wikiCallback';
+
+            $.ajax({
+                    url: wikiUrl,
+                    dataType: dt,
+                    success: function(response){
+                        var titleList = response[1],
+                            definitionList = response[2];
+
+                        for (var i = 0; i < titleList.length; i++) {
+                            var titleStr = titleList[i],
+                                urlStr = 'http://en.wikipedia.org/wiki/' + titleStr,
+                                definitionStr = definitionList[i];
+                            self.links.push({url: urlStr, title: titleStr, definition: definitionStr});
+                        }
+                    clearTimeout(wikiRequestLinksTimeout);
+                }
+            });
+        });
+    };
+
+
+	var AppViewModel = function() {
+	    self = this;
+
+	    self.wikipediaLinks = new GetWikiLinks().links;
+
+    };
+
+ko.applyBindings(new AppViewModel());
+}());
